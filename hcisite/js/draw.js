c40352d5a069e2eb78e73a6ff951eb0deac4aa1d
Sean Hamilton <skhamilt@eng.ucsd.edu>
2017-06-02 17:58:27 -0700

    submitting changes to merge

--- hcisite/js/draw.js
+++ hcisite/js/draw.js
@@ -2,9 +2,24 @@
  * Created by sean on 5/29/17.
  */
 
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
+
 function redraw()
 {
     var Total = 0;
+    var hasTotal = getQueryVariable("Total");
+    if (hasTotal != false) {
+        Total = hasTotal;
+    }
 	Total = Total + (document.getElementById("hvac").value)*10;
 	document.getElementById("Total").value = Total;
 	draw(Total);
